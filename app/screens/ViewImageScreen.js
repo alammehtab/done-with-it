c39553b87626d15f4cc3e5fa2ab3800f3c9a9b32
Mehtab Alam <mehtaba728@mail.com>
2021-09-28 12:56:14 +0500

    added the close and delete icons

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,26 +1,44 @@
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, SafeAreaView, View } from "react-native";
 
 function ViewImageScreen(props) {
   return (
-    <View style={styles.imageContainer}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.closeIcon} />
+      <View style={styles.deleteIcon} />
       <Image
         resizeMode="contain"
         style={styles.image}
         source={require("../assets/chair.jpg")}
       />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#fc5c65",
+    position: "absolute",
+    top: 30,
+    left: 30,
+  },
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: "#4ecdc4",
+    position: "absolute",
+    top: 30,
+    right: 30,
+  },
   image: {
     width: "100%",
     height: "100%",
-  },
-  imageContainer: {
-    backgroundColor: "black",
-    flex: 1,
   },
 });
 export default ViewImageScreen;
